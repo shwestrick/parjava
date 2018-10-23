bdc07dee786e99ae5f2e1564bef3d20c45986a3f
Rohan Yadav <rohany@Rohans-MacBook-Pro.local>
2018-10-22 23:28:22 -0400

    moving everything ebcause i don't actually know how java works

--- java-benchmarks/Runner.java
+++ java-benchmarks/Runner.java
@@ -4,9 +4,6 @@ import java.util.function.*;
 class Runner {
     static final long NPS = (1000L * 1000 * 1000);
     public static void run (Function<Void, Void> func, int reps) {
-        Void v = null;
-        func.apply(v);
-
         for (int r = 0;r < reps;r++) {
             long start = System.nanoTime();
             func.apply(null);
@@ -14,6 +11,19 @@ class Runner {
             double elapsed = (double) (end - start) / NPS;
             System.out.printf("Time: %7.3f\n", elapsed);
         }
+    }
+
+    public static void run_with_setup(Function<Void, Void> setup,
+                                      Function<Void, Void> func, int reps) {
+        for (int r = 0;r < reps;r++) {
+            setup.apply(null);
+            long start = System.nanoTime();
+            func.apply(null);
+            long end = System.nanoTime();
+            double elapsed = (double) (end - start) / NPS;
+            System.out.printf("Time: %7.3f\n", elapsed);
+        }
+
     }
 
     public static void test() {
